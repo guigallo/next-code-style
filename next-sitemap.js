09fcb9eb662ba8fbe84bc8be89008ef2dd5b849e
Guilherme <guilherme.gg1@hotmail.com>
2021-09-11 00:05:24 -0300

    refactor: remove default next files

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,10 +1,8 @@
 module.exports = {
-  siteUrl: 'https://genesys-dev.vercel.app/',
+  siteUrl: 'www.yourwebsite.com',
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'], // <= exclude here
+  exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://genesys-dev.vercel.app/server-sitemap.xml', // <==== Add here
-    ],
+    additionalSitemaps: ['/genesys-dev.vercel.app/server-sitemap.xml'],
   },
 }
